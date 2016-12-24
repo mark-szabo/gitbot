231102888ae1c39abdc30e20596727035d8685e5
Mark Szabo <szmaorka@gmail.com>
2016-12-24 13:13:48 +0100

    broadcast

--- gitbot/Dialogs/RootDialog.cs
+++ gitbot/Dialogs/RootDialog.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Microsoft.Bot.Connector;
 
 namespace gitbot.Dialogs
@@ -20,20 +16,26 @@ namespace gitbot.Dialogs
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
             var message = await result;
-            await Broadcast(new Uri(message.ServiceUrl), message.Conversation);
+            //await Broadcast(new Uri(message.ServiceUrl), message.Conversation, message.Recipient);
+            await Broadcast(
+                $"ServiceUrl: {message.ServiceUrl} \n ConversationId: {message.Conversation.Id} \n BotAccountId: {message.Recipient.Id} \n BotAccountName: {message.Recipient.Name}",
+                "https://facebook.botframework.com/", 
+                "1068190953300282-283726938688635", 
+                "283726938688635", 
+                "gitbot_84AlOPcgTdB" );
             context.Wait(MessageReceivedAsync);
         }
 
-        public async Task Broadcast(Uri serviceUrl, ConversationAccount conversation)
+        public async Task Broadcast(string text, string serviceUrl, string conversationId, string botAccountId, string botAccountName)
         {
-            /*var connector = new ConnectorClient(serviceUrl);
+            var connector = new ConnectorClient(new Uri(serviceUrl));
             IMessageActivity newMessage = Activity.CreateMessageActivity();
             newMessage.Type = ActivityTypes.Message;
-            newMessage.From = botAccount;
-            newMessage.Conversation = conversation;
-            newMessage.Recipient = userAccount;
-            newMessage.Text = "Yo yo yo!";
-            await connector.Conversations.SendToConversationAsync((Activity)newMessage);*/
+            newMessage.From = new ChannelAccount { Id = botAccountId, Name = botAccountName};
+            newMessage.Conversation = new ConversationAccount { Id = conversationId};
+            newMessage.Recipient = new ChannelAccount { Id = conversationId, Name = null};
+            newMessage.Text = text;
+            await connector.Conversations.SendToConversationAsync((Activity)newMessage);
         }
     }
 }
